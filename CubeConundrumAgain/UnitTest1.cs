e92606c88efde19c14c41419bf7df96b8c1e293a
RGV <ricardo.guzman.velasco@outlook.com>
2024-02-09 19:16:00 +0100

    askldj

--- CubeConundrumAgain/UnitTest1.cs
+++ CubeConundrumAgain/UnitTest1.cs
@@ -11,21 +11,33 @@ public class Tests
             .Handful(1, 23, 3)
             .Handful(2, 123, 323)
             .IsPossible.Should().BeTrue();
+        
+        Game.safasas("1", 0, 0, 0)
+            .Handful(1, 0, 0)
+            .IsPossible.Should().BeFalse();
     }
 }
 
 public class Game
 {
+    private (int r, int g, int b) bag;
+    private List<(int r, int g, int b)> handfuls = new();
+
+    public Game(string id, (int r, int g, int b) bag)
+    {
+        this.bag = bag;
+    }
 
     public static Game safasas(string s, int i, int i1, int i2)
     {
-        throw new NotImplementedException();
+        return new Game(s, (i, i1, i2));
     }
 
     public Game Handful(int i, int i1, int i2)
     {
-        throw new NotImplementedException();
+        handfuls.Add((i, i1, i2));
+        return this;
     }
 
-    public bool IsPossible => true;
+    public bool IsPossible => handfuls.All(h => h.r <= bag.r && h.g <= bag.g && h.b <= bag.b);
 }
