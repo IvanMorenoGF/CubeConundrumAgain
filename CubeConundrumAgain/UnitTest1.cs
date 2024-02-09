38a21830e970715783e7833b81135cb905f2b53d
Iván Moreno <imoreno.owo@gmail.com>
2024-02-09 19:27:39 +0100

    feat: return 0 when there are no games

--- CubeConundrumAgain/UnitTest1.cs
+++ CubeConundrumAgain/UnitTest1.cs
@@ -7,18 +7,37 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Game.safasas("1", new(1231,12312,123131))
+        Game.safasas(1, new(1231,12312,123131))
             .Handful(1, 23, 3)
             .Handful(2, 123, 323)
             .IsPossible.Should().BeTrue();
         
-        Game.safasas("1", new(0, 0, 0))
+        Game.safasas(1, new(0, 0, 0))
             .Handful(1, 0, 0)
             .IsPossible.Should().BeFalse();
 
-        Game.safasas("1", new(1, 1, 1))
+        Game.safasas(1, new(1, 1, 1))
             .Handful(1, 1, 1)
             .IsPossible.Should().BeTrue();
+    }
+
+    [Test]
+    public void afsadf()
+    {
+        Asfasfs.NewOne().Result().Should().Be(0);
+    }
+}
+
+public class Asfasfs
+{
+    public static Asfasfs NewOne()
+    {
+        return new();
+    }
+
+    public int Result()
+    {
+        return 0;
     }
 }
 
@@ -43,20 +62,22 @@ public class Game
 {
     private Bag bag;
     private List<Bag> handfuls = new();
+    int id;
 
-    internal Game(string id, Bag bag)
+    internal Game(int id, Bag bag)
     {
+        this.id = id;
         this.bag = bag;
     }
 
-    internal static Game safasas(string s, Bag whereStore)
+    internal static Game safasas(int id, Bag whereStore)
     {
-        return new Game(s, whereStore);
+        return new Game(id, whereStore);
     }
 
-    public Game Handful(int i, int i1, int i2)
+    public Game Handful(int r, int g, int b)
     {
-        handfuls.Add(new(i, i1, i2));
+        handfuls.Add(new(r, g, b));
         return this;
     }
 
