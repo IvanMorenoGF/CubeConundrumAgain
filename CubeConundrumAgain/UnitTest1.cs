d8ed575fd8a07ad8978b370d74428fd983a0d04b
Iván Moreno <imoreno.owo@gmail.com>
2024-02-09 19:10:12 +0100

    feat: is game possible

--- CubeConundrumAgain/UnitTest1.cs
+++ CubeConundrumAgain/UnitTest1.cs
@@ -1,15 +1,31 @@
+using FluentAssertions;
+
 namespace CubeConundrumAgain;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        Game.safasas("1", 1231,12312,123131)
+            .Handful(1, 23, 3)
+            .Handful(2, 123, 323)
+            .IsPossible.Should().BeTrue();
     }
+}
+
+public class Game
+{
+
+    public static Game safasas(string s, int i, int i1, int i2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Game Handful(int i, int i1, int i2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsPossible => true;
 }
