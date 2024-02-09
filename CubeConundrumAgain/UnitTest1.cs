5b235c24f38da064422193e5e829dd0140038062
Iván Moreno <imoreno.owo@gmail.com>
2024-02-09 19:17:38 +0100

    refactor:  adsfasf

--- CubeConundrumAgain/UnitTest1.cs
+++ CubeConundrumAgain/UnitTest1.cs
@@ -7,30 +7,44 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Game.safasas("1", 1231,12312,123131)
+        Game.safasas("1", new(1231,12312,123131))
             .Handful(1, 23, 3)
             .Handful(2, 123, 323)
             .IsPossible.Should().BeTrue();
         
-        Game.safasas("1", 0, 0, 0)
+        Game.safasas("1", new(0, 0, 0))
             .Handful(1, 0, 0)
             .IsPossible.Should().BeFalse();
     }
 }
 
+public struct Bag
+{
+    public readonly int r;
+    public readonly int g;
+    public readonly int b;
+
+    public Bag(int r, int g, int b)
+    {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
+
 public class Game
 {
-    private (int r, int g, int b) bag;
+    private Bag bag;
     private List<(int r, int g, int b)> handfuls = new();
 
-    public Game(string id, (int r, int g, int b) bag)
+    public Game(string id, Bag bag)
     {
         this.bag = bag;
     }
 
-    public static Game safasas(string s, int i, int i1, int i2)
+    public static Game safasas(string s, Bag whereStore)
     {
-        return new Game(s, (i, i1, i2));
+        return new Game(s, whereStore);
     }
 
     public Game Handful(int i, int i1, int i2)
