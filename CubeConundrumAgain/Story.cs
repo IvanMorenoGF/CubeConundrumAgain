295e4e689255e34067c5428fb57436971548239b
RGV <ricardo.guzman.velasco@outlook.com>
2024-02-10 12:00:56 +0100

    refactor: use lst

--- CubeConundrumAgain/Story.cs
+++ CubeConundrumAgain/Story.cs
@@ -1,15 +1,16 @@
 using LanguageExt;
+using static LanguageExt.Lst<LanguageExt.Option<CubeConundrumAgain.Scene>>;
 
 namespace CubeConundrumAgain;
 
 public class Story
 {
-    IEnumerable<Option<Scene>> allScenes;
-    Story(Option<Scene> scene) => this.allScenes = new[]{scene};
-    Story(IEnumerable<Option<Scene>> scene) => this.allScenes = scene;
+    readonly Lst<Option<Scene>> allScenes;
+    Story(Option<Scene> scene) => allScenes = Empty.Add(scene);
+    Story(Lst<Option<Scene>> scene) => this.allScenes = scene;
     public static Story OnceUponATime() => new(Option<Scene>.None);
     public bool IsAlive(string who) => allScenes.Last().Match(where => !where.IsInTheTomb(who), () => true);
-    public Story Happened(Option<Scene> what) => new(allScenes.Append(what));
+    public Story Happened(Option<Scene> what) => new(allScenes.Add(what));
 
     public bool Loves(string from, string to) 
         => allScenes.First(x => x.IsSome).Match
