4df27c475915c1962d4edd24bc4de7bec5baeffb
Iván Moreno <imoreno.owo@gmail.com>
2024-02-10 10:47:13 +0100

    feat: adan is alive by default

--- CubeConundrumAgain/UnitTest1.cs
+++ CubeConundrumAgain/UnitTest1.cs
@@ -7,6 +7,19 @@ public class Tests
     [Test]
     public void Test1()
     {
-        
+        Story.ASdfsaf().IsAdanAlive().Should().BeTrue();
+    }
+}
+
+public class Story
+{
+    public static Story ASdfsaf()
+    {
+        return new();
+    }
+
+    public bool IsAdanAlive()
+    {
+        return true;
     }
 }
