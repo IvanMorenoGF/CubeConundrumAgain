6cf5211d0c7b6c1687d11c8c70ae4179242dddb0
RGV <ricardo.guzman.velasco@outlook.com>
2024-02-10 11:56:45 +0100

    refactor: compare people

---
+++ CubeConundrumAgain/Character.cs
@@ -0,0 +1,14 @@
+﻿namespace CubeConundrumAgain;
+
+public class Character
+{
+    private readonly string name;
+
+    public Character(string name) => this.name = name;
+    
+    public static bool operator ==(Character left, Character right) => left.name == right.name;
+    public static bool operator !=(Character left, Character right) => !(left == right);
+
+    public static implicit operator string(Character character) => character.name;
+    public static implicit operator Character(string name) => new(name);
+}
--- CubeConundrumAgain/Scene.cs
+++ CubeConundrumAgain/Scene.cs
@@ -3,50 +3,33 @@ namespace CubeConundrumAgain;
 public class Scene
 {
     readonly string buriedOne;
-    readonly (string theOne, string theOther) couple = ("Adan", "Eva");
+    readonly (Character theOne, Character theOther) couple = ("Adan", "Eva");
     private readonly string sceneId;
 
-    public Scene(string buriedOne)
+    public Scene(Character buriedOne)
     {
         this.buriedOne = buriedOne;
         this.sceneId = "Death";
     }
 
-    public Scene(string adan, string eva)
+    public Scene(Character adan, Character eva)
     {
         couple = (theOne: adan, theOther: eva);
         this.sceneId = "Love";
     }
 
-    public static Scene Death()
-    {
-        return new("");
-    }
+    public static Scene Death() => new("");
+    public static Scene Love() => new("");
 
-    public Scene Buried(string who)
-    {
-        return new(who);
-    }
+    public Scene Buried(Character who) => new(who);
+    public bool IsInTheTomb(Character who) => buriedOne == who;
 
-    public bool IsInTheTomb(string who)
-    {
-        return buriedOne == who;
-    }
+    public Scene Between(Character one, Character other) => new(one, other);
 
-    public static Scene Love()
-    {
-        return new("");
-    }
-
-    public Scene Between(string adan, string eva)
-    {
-        return new(adan, eva);
-    }
-    
     public bool IsLoveScene => sceneId == "Love";
     public bool IsDeathScene => sceneId == "Death";
 
-    public string LoverOf(string lover) => couple.theOne == lover ? couple.theOther : couple.theOne;
-    public bool AreCoupled(string one, string other) => IsInTheCast(one) && IsInTheCast(other) && IsLoveScene;
-    public bool IsInTheCast(string who) => who == couple.theOne || who == couple.theOther;
+    public string LoverOf(Character lover) => couple.theOne == lover ? couple.theOther : couple.theOne;
+    public bool AreCoupled(Character one, Character other) => IsInTheCast(one) && IsInTheCast(other) && IsLoveScene;
+    public bool IsInTheCast(Character who) => who == couple.theOne || who == couple.theOther;
 }
