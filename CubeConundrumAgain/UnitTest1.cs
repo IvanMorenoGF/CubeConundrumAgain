4e237220af44494572242c5aa46a4d86427552b9
Iván Moreno <imoreno.owo@gmail.com>
2024-02-10 10:48:51 +0100

    feat: keep everybody alive at story start

--- CubeConundrumAgain/UnitTest1.cs
+++ CubeConundrumAgain/UnitTest1.cs
@@ -8,6 +8,8 @@ public class Tests
     public void Test1()
     {
         Story.ASdfsaf().IsAdanAlive().Should().BeTrue();
+        Story.ASdfsaf().IsEvaAlive().Should().BeTrue();
+        Story.ASdfsaf().IsAlive("Adan").Should().BeTrue();
     }
 }
 
@@ -19,6 +21,16 @@ public class Story
     }
 
     public bool IsAdanAlive()
+    {
+        return IsAlive("Adan");
+    }
+
+    public bool IsEvaAlive()
+    {
+        return IsAlive("Eva");
+    }
+
+    public bool IsAlive(string who)
     {
         return true;
     }
