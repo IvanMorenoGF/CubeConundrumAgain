755071bb6a3184a8305d5be0aad8705b861bbc81
RGV <ricardo.guzman.velasco@outlook.com>
2024-02-17 12:14:32 +0100

    feat: drag character to scene

--- FunctionalStoryteller.Tests/SceneCommandsTests.cs
+++ FunctionalStoryteller.Tests/SceneCommandsTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using static FunctionalStoryteller.Commands;
+using static FunctionalStoryteller.Scene;
 using static FunctionalStoryteller.StoryBoard;
 using static FunctionalStoryteller.Tests.CharactersToTestsWith;
 
@@ -11,5 +12,11 @@ public class SceneCommandsTests
     public void DragCharacter_ToVignette_DoesNothing()
     {
         DragTo(vignette: 1, Adam).SketchIn(Blank(1)).Should().Be(Blank(1));
+    }
+
+    [Test]
+    public void DragCharacter_ToScene()
+    {
+        DragTo(vignette: 1, Adam).SketchIn(1.Vignettes().In1(Solitude())).Should().NotBe(Blank(1).In1(Solitude()));
     }
 }
