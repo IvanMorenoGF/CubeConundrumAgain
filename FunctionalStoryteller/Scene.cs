b9da9146bb61370971f747323c6f12dd571dae42
RGV <ricardo.guzman.velasco@outlook.com>
2024-02-18 17:37:12 +0100

    refactor: arrange scene methods

--- FunctionalStoryteller/Scene.cs
+++ FunctionalStoryteller/Scene.cs
@@ -6,8 +6,8 @@ public abstract record Scene
 {
     public bool IsInTheCast(Character who) => Cast.Contains(who);
     public bool AreInTheCast(params Character[] who) => who.All(IsInTheCast);
+    public Character CharacterAt(int index) => Cast[index - 1];
+    
     protected abstract Seq<Character> Cast { get; }
     public abstract Scene PlaceAt(int where, Character who);
-    
-    public Character CharacterAt(int index) => Cast[index - 1];
 }
