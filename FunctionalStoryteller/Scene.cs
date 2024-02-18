c97b25f3fe7cdaa95c510857c36467602426c11f
RGV <ricardo.guzman.velasco@outlook.com>
2024-02-18 17:37:48 +0100

    refactor: require index of character is ok

--- FunctionalStoryteller/Scene.cs
+++ FunctionalStoryteller/Scene.cs
@@ -6,8 +6,12 @@ public abstract record Scene
 {
     public bool IsInTheCast(Character who) => Cast.Contains(who);
     public bool AreInTheCast(params Character[] who) => who.All(IsInTheCast);
-    public Character CharacterAt(int index) => Cast[index - 1];
-    
+    public Character CharacterAt(int index)
+    {
+        if (index < 1 || index > Cast.Count) throw new ArgumentOutOfRangeException(nameof(index));
+        return Cast[index - 1];
+    }
+
     protected abstract Seq<Character> Cast { get; }
     public abstract Scene PlaceAt(int where, Character who);
 }
