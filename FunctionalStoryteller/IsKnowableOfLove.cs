1c26eaea9cf8f1dd452ed636bceb9d474e32d477
Iván Moreno <imoreno.owo@gmail.com>
2024-03-02 12:13:49 +0100

    feat: cannot fall in love with ghost

--- FunctionalStoryteller/IsKnowableOfLove.cs
+++ FunctionalStoryteller/IsKnowableOfLove.cs
@@ -6,7 +6,10 @@ public class IsKnowableOfLove : Spec
     public IsKnowableOfLove(Character who) => this.who = who;
 
     public override bool IsSatisfiedBy(Story story)
-        => story.asfsafasf().Any(substory => substory.All<LoveScene>(of: who).Any(WasInLoveWithSomeone));
+        => story.asfsafasf().Any(substory => substory.All<LoveScene>(of: who).Any(loveScene => WasInLoveWithSomeone(loveScene, substory)));
 
-    bool WasInLoveWithSomeone(LoveScene scene) => scene.PotentialLoverOf(who).IsSome;
+    bool WasInLoveWithSomeone(LoveScene scene, Story substory)
+    {
+        return scene.PotentialLoverOf(who).IsSome && substory.Is(Specs.Alive(scene.LoverOf(who)));
+    }
 }
