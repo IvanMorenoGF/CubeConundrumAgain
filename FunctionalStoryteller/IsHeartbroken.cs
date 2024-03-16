838487291f83292ef25c94da3bd4e9d5ffe1f059
Iván Moreno <imoreno.owo@gmail.com>
2024-03-16 11:07:15 +0100

    refactor: inline isheartbroken method content

--- FunctionalStoryteller/IsHeartbroken.cs
+++ FunctionalStoryteller/IsHeartbroken.cs
@@ -1,3 +1,4 @@
+using LanguageExt;
 using LanguageExt.UnsafeValueAccess;
 
 namespace FunctionalStoryteller;
@@ -7,17 +8,10 @@ public class IsHeartbroken : Spec
     readonly Character who;
     public IsHeartbroken(Character who) => this.who = who;
 
-    public override bool IsSatisfiedBy(Story story)
-    {
-        var firstLove = story.FirstLoveOf(who);
-        if (firstLove.IsNone) return false;
-        if (!story.scenes.OfType<DeathScene>().Any(x => x.IsInTheTomb(firstLove.ValueUnsafe()) && x.IsInTheCast(who)))
-            return false;
-
-        var lastMeetingWithLove = story.scenes.LastOrDefault(x => x.IsInTheCast(who) && x.IsInTheCast(firstLove.ValueUnsafe()));
-        var deathOfLover = story.scenes.OfType<DeathScene>().LastOrDefault(x => x.IsInTheTomb(firstLove.ValueUnsafe()));
-        var theLater = story.TheLater(lastMeetingWithLove, deathOfLover);
-
-        return theLater == deathOfLover;
-    }
+    public override bool IsSatisfiedBy(Story story) 
+        => story.AreInOrder
+        (
+            story.scenes.LastOrDefault(x => x.IsInTheCast(who) && x.IsInTheCast(story.FirstLoveOf(who).ValueUnsafe())),
+            story.scenes.OfType<DeathScene>().LastOrDefault(x => x.IsInTheTomb(story.FirstLoveOf(who).ValueUnsafe()) && x.IsInTheCast(who))
+        );
 }
