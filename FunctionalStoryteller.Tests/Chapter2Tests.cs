34736bb4757c5ebfe549423c66ae1788d45a1518
Iván Moreno <imoreno.owo@gmail.com>
2024-03-30 10:18:06 +0100

    test: write chapter 2 level 1 requirements

--- FunctionalStoryteller.Tests/Chapter2Tests.cs
+++ FunctionalStoryteller.Tests/Chapter2Tests.cs
@@ -1,3 +1,9 @@
+using FluentAssertions;
+using static FunctionalStoryteller.Scenes;
+using static FunctionalStoryteller.Specs;
+using static FunctionalStoryteller.Storyteller;
+using static FunctionalStoryteller.Tests.CharactersToTestsWith;
+
 namespace FunctionalStoryteller.Tests;
 
 public class Chapter2Tests
@@ -14,5 +20,11 @@ public class Chapter2Tests
         // Se da el amor entre Adam y Eve
         // Se da el amor entre Adam y Enriqueto
         // Se da el amor entre Enriqueto y otra persona (que no sea Adam ni Eve)
+
+        OnceUponATime()
+            .Happened(Love().Between(Adam, Eve))
+            .Happened(Death().Of(Adam).WatchedBy(Eve))
+            .Happened(Love().Between(Eve, AnybodyElse))
+            .Then(InLoveWith(Eve, AnybodyElse)).Should().BeTrue();
     }
 }
