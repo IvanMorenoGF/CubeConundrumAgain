c6c62482429e6ccf6576b648cb68dc4d5d6dcc43
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-06-30 16:44:22 +0200

    feat: create 2048 game from string

--- Coding2048.Tests/UnitTest1.cs
+++ Coding2048.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace Coding2048.Tests;
 
 public class Tests
@@ -5,6 +7,19 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        Game2048.From("0").At(0,0).Should().Be(0);
+    }
+}
+
+public class Game2048
+{
+    public static Game2048 From(string asfsafas)
+    {
+        return new();
+    }
+
+    public int At(int x, int y)
+    {
+        return 0;
     }
 }
