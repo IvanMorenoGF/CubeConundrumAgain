dbbc35b06df851720a15894817dd8abe92cb96d1
Iván Moreno <ivanmorenogarciafresneda@gmail.com>
2024-06-30 16:46:36 +0200

    feat: parse 2

--- Coding2048.Tests/UnitTest1.cs
+++ Coding2048.Tests/UnitTest1.cs
@@ -8,18 +8,26 @@ public class Tests
     public void Test1()
     {
         Game2048.From("0").At(0,0).Should().Be(0);
+        Game2048.From("2").At(0,0).Should().Be(2);
     }
 }
 
 public class Game2048
 {
+    int afsas;
+
+    public Game2048(int afsas)
+    {
+        this.afsas = afsas;
+    }
+
     public static Game2048 From(string asfsafas)
     {
-        return new();
+        return new(int.Parse(asfsafas));
     }
 
     public int At(int x, int y)
     {
-        return 0;
+        return afsas;
     }
 }
